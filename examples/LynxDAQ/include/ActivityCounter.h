b5fa45c3258682f29af2167e54a79f2134ea6535
Ben Yee <benyee@berkeley.edu>
2012-05-16 12:38:16 -0700

    Wrote part of framework of ActivityCounter

--- examples/LynxDAQ/include/ActivityCounter.h
+++ examples/LynxDAQ/include/ActivityCounter.h
@@ -1,4 +1,35 @@
 #ifndef ACTIVITYCOUNTER_H
 #define ACTIVITYCOUNTER_H
 
+class ActivityCounter{
+public:
+    ActivityCounter(int input_ROIL, int input_ROIR,int input_eventsPerBin = 50,double input_dt = 1);
+    ~ActivityCounter();
+
+private:
+    int leftROI;
+    int rightROI;
+
+    int eventsPerBin;
+    double dt;
+    bool binMode;
+
+    vector<double> energy;
+    vector<double> timestamp;
+
+    vector<double> rate;
+    vector<double> time;
+
+    void setMode(bool x){binMode = x;}
+    bool getMode(){return binMode;}
+    void setBinRate(double x);
+    double getBinRate();
+
+    void resetLROI(int x){leftROI = x;}
+    void resetRROI(int x){rightROI = x;}
+    int* getROI(int x){int* out = {leftROI, rightROI}; return out;}
+
+    void addToList(vector<double> e, vector<double> t);
+};
+
 #endif // ACTIVITYCOUNTER_H
--- examples/LynxDAQ/src/ActivityCounter.cpp
+++ examples/LynxDAQ/src/ActivityCounter.cpp
@@ -1,1 +1,25 @@
 #include "ActivityCounter.h"
+
+ActivityCounter::ActivityCounter(int input_ROIL, int input_ROIR,int input_eventsPerBin,double input_dt){
+    leftROI = input_ROIL;
+    rightROI = input_ROIR;
+
+    eventsPerBin = input_eventsPerBin;
+    dt = input_dt;
+}
+ActivityCounter::~ActivityCounter(){}
+
+
+ActivityCounter::getBinRate(){
+    if(binMode){return dt;}
+    return (double)eventsPerBin;
+}
+ActivityCounter::setBinRate(double x){
+    eventsPerBin = (int)x;
+    dt = x;
+}
+
+void addToList(vector<double> e, vector<double> t){
+    energy.insert(energy.end(),e.begin(),e.end());
+    timestamp.insert(timestamp.end(),t.begin(),t.end());
+}
